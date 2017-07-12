af42b7c5be9efcf416bb0077d4a7596edc8698e3
Jennifer Volpe <jennifervolpe@Jennifers-MBP.home>
2017-07-11 20:47:11 -0400

    block

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,8 @@
-def hello_t
-
-end
-
-# call your method here!
-
+def hello_t(array)
+        i = 0
+      while i < array.length
+        yield(array[i])
+       i = i + 1
+     end
+      array
+end		
